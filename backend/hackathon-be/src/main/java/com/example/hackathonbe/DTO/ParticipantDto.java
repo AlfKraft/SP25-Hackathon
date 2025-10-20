ce25201bb527e46798122225fe748c55c97dfd69
AlfKraft <124708277+AlfKraft@users.noreply.github.com>
2025-10-20 14:10:01 +0300

    Get all participants API (#40)
    Co-authored-by: HGRUPP\alfred.kraft <alfred.kraft@helmes.com>

--- backend/hackathon-be/src/main/java/com/example/hackathonbe/DTO/ParticipantDto.java
+++ backend/hackathon-be/src/main/java/com/example/hackathonbe/DTO/ParticipantDto.java
@@ -1,4 +1,15 @@
 package com.example.hackathonbe.DTO;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.Data;
+
+@Data
 public class ParticipantDto {
+
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private JsonNode data;
+
 }
---
+++ backend/hackathon-be/src/main/java/com/example/hackathonbe/controllers/ParticipantController.java
@@ -0,0 +1,24 @@
+package com.example.hackathonbe.controllers;
+
+import com.example.hackathonbe.DTO.ParticipantDto;
+import com.example.hackathonbe.services.ParticipantService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/participants")
+@RequiredArgsConstructor
+public class ParticipantController {
+
+    private final ParticipantService participantService;
+
+    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<ParticipantDto> getAllParticipants() {
+        return participantService.getAllParticipants();
+    }
+}
